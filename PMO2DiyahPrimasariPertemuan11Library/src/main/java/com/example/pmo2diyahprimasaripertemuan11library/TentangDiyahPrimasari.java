1b18ead6acbb083c7eccce25f6a8652b66e01e2d
DiyahPrimasari0289 <64175279+DiyahPrimasari0289@users.noreply.github.com>
2021-01-04 13:35:56 +0700

    PMO2DiyahPrimasariPertemuan11

---
+++ PMO2DiyahPrimasariPertemuan11Library/src/main/java/com/example/pmo2diyahprimasaripertemuan11library/TentangDiyahPrimasari.java
@@ -0,0 +1,14 @@
+package com.example.pmo2diyahprimasaripertemuan11library;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TentangDiyahPrimasari extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang_diyah_primasari);
+    }
+}
